b20134488efac28f03e400b39c2cd4655813a4b0
JubeatWwW <l6104400@gmail.com>
2020-04-22 15:55:36 +0800

    ✨ Feat: Add more search methods and vscode runner

--- Elementary Data Structures/Array/main.cpp
+++ Elementary Data Structures/Array/main.cpp
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <iostream>
 #include "array_rotation/array_rotation.hpp"
 #include "sorted_and_rotated_array_search/search.hpp"
+using namespace std;
+using namespace sorted_and_rotated_array_search;
 
 int main() {
     int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
@@ -14,6 +17,14 @@ int main() {
             printf("%d ", arr[i]);
         }
         printf("\n");
+        int position = single(arr, 8, 3);
+        cout << "found 3 at position: " << position << endl;
+        position = single(arr, 8, 4);
+        cout << "found 4 at position: " << position << endl;
+        position = single_improved(arr, 8, 3);
+        cout << "found 3 at position: " << position << endl;
+        position = single_improved(arr, 8, 4);
+        cout << "found 4 at position: " << position << endl;
     }
 
     return 0;
--- Elementary Data Structures/Array/sorted_and_rotated_array_search/search.hpp
+++ Elementary Data Structures/Array/sorted_and_rotated_array_search/search.hpp
@@ -1,6 +1,9 @@
 #ifndef H_SEARCH_HPP
 #define H_SEARCH_HPP
 
+#include <algorithm>
+using namespace std;
+
 namespace sorted_and_rotated_array_search {
     template<typename T, typename LN = int>
     LN find_pivot(T arr[], LN l, LN h) {
@@ -39,7 +42,7 @@ namespace sorted_and_rotated_array_search {
             return mid;
         }
 
-        if (arr[mid] > target) {
+        if (arr[mid] < target) {
             return bsearch(arr, (mid + 1), h, target);
         }
 
@@ -60,7 +63,7 @@ namespace sorted_and_rotated_array_search {
             return pivot;
         }
 
-        if (arr[pivot] > target) {
+        if (arr[0] <= target) {
             return bsearch(arr, 0, (pivot - 1), target);
         }
 
@@ -155,6 +158,59 @@ namespace sorted_and_rotated_array_search {
             }
         }
         return false;
+    }
+
+    // https://www.geeksforgeeks.org/find-maximum-value-of-sum-iarri-with-only-rotations-on-given-array-allowed/
+    /*
+        R_0 = 0*arr[0] + 1*arr[1] + 2*arr[2] + ... + (n-2)*arr[n-1] + (n-1)*arr[n-1]
+        R_1 = 0*arr[n-1] + 1*arr[0] + 2*arr[1] + 3*arr[2] + ... + (n-1)*arr[n-2]
+        R_2 = 0*arr[n-2] + 1*arr[n-1] + 2*arr[0] + 3*arr[1] + ... + (n-1)*arr[n-3]
+
+        R_1 - R_0 = arr[0] + arr[1] + ... + arr[n-2] - (n-1)*arr[n-1]
+                  = (arr[0] + arr[1] + ... + arr[n-2] + arr[n-1]) - n*arr[n-1]
+        R_2 - R_1 = arr[0] + arr[1] + ... + arr[n-3] - (n-1)*arr[n-2] + arr[n-1]
+                  = arr[0] + arr[1] + ... + arr[n-3] + (arr[n-2] - n*arr[n-2]) + arr[n-1]
+                  = (arr[0] + arr[1] + ... + arr[n-1]) - n*arr[n-2]
+
+        R_j - R_(j-1) = (arr[0] + arr[1] + ... + arr[n-1]) - n*arr[n-j]
+        R_j = arr_sum - n*arr[n-j] + R_(j-1)
+
+        max_sum = min(arr[n-j])
+    */
+    template<typename T, typename LN = int>
+    LN max_sum_of_rotations(T arr[], LN n) {
+        LN sum = 0;
+        LN mul_sum = 0;
+
+        // calc R_0
+        for (LN i = 0; i < n; i += 1) {
+            sum += arr[i];
+            mul_sum += i * arr[i];
+        }
+
+        LN max_sum = mul_sum;
+        for (LN i = 1; i < n; i += 1) {
+            // R_j = R_(j-1) + arr_sum - n*arr[n-j]
+            mul_sum += (sum - n * arr[n-i]);
+            max_sum = max(max_sum, mul_sum);
+        }
+
+        return max_sum;
+    }
+
+    // https://www.geeksforgeeks.org/maximum-sum-iarri-among-rotations-given-array/
+    // method 3
+    // As we know for an array the maximum sum will be when the array is sorted in ascending order.
+    template<typename T, typename LN = int>
+    LN max_sum_of_sorted_rotated(T arr[], LN n) {
+        LN pivot = find_pivot(arr, 0, n - 1);
+        LN sum = 0;
+
+        LN lowest_index = (pivot + 1) % n;
+        for (LN i = 0; i < n; i += 1) {
+            sum += (i * arr[(i + lowest_index) % n]);
+        }
+        return sum;
     }
 }
 
